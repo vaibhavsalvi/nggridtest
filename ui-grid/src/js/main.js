69e5e805db250c70c54d51cf70e6dd510c375b9a
vaibhavsalvi <vaibhav.v.salvi@gmail.com>
2016-05-08 15:06:46 +0530

    ADD readme file AAA
    ADD readme file AAA

--- ui-grid/src/js/main.js
+++ ui-grid/src/js/main.js
@@ -1,9 +1,15 @@
 var app = angular.module('myApp', ['ui.grid']);
-app.controller('MyCtrl', function($scope) {
-    $scope.myData = [{name: "Moroni", age: 50},
-                     {name: "Tiancum", age: 43},
-                     {name: "Jacob", age: 27},
-                     {name: "Nephi", age: 29},
-                     {name: "Enos", age: 34}];
-    $scope.gridOptions = { data: 'myData' };
+app.controller('MyCtrl', function($scope,$http) {
+console.log('hj');
+    $http.jsonp('http://localhost:8081?alt=json-in-script&callback=JSON_CALLBACK').
+       success(function(json) {
+             $scope.myData=json;
+			 $scope.gridOptions = { data: 'myData' };
+        });
+   // $scope.myData = [{name: "Vaibhav", age: 50},
+                    // {name: "Tiancum", age: 43},
+                    // {name: "Jacob", age: 27},
+                    // {name: "Nephi", age: 29},
+                    // {name: "Enos", age: 34}];
+    
 });
